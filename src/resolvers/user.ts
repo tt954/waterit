faeec3b86c332c4e3344eae7df975d7f34a13e38
tieulam <ttl.t954@gmail.com>
2020-09-26 15:48:17 -0600

    add me query in user resolver

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,6 +1,6 @@
 import { User } from "../entities/User";
 import { MyContext } from "src/types";
-import { Resolver, Mutation, InputType, Field, Arg, Ctx, ObjectType } from "type-graphql";
+import { Resolver, Mutation, InputType, Field, Arg, Ctx, ObjectType, Query } from "type-graphql";
 import argon2 from 'argon2';
 
 @InputType()
@@ -31,6 +31,18 @@ class UserResponse {
 
 @Resolver()
 export class UserResolver {
+  @Query(() => User, { nullable: true })
+  async me(
+    @Ctx() { em, req }: MyContext
+  ) {
+    if (!req.session.userId) {
+      return null // not logged in 
+    }
+
+    const user = await em.findOne(User, { id: req.session.userId })
+    return user; 
+  }
+
   @Mutation(() => UserResponse)
   async register(
     @Arg('options') options: UsernamePasswordInput,
@@ -77,7 +89,7 @@ export class UserResolver {
   @Mutation(() => UserResponse)
   async login(
     @Arg('options') options: UsernamePasswordInput,
-    @Ctx() { em }: MyContext
+    @Ctx() { em, req }: MyContext
   ) : Promise<UserResponse> {
     const user = await em.findOne(User, { username: options.username });
     if (!user) {
@@ -100,9 +112,9 @@ export class UserResolver {
         ],
       };
     }
+
+    req.session.userId = user.id; 
     
-    return {
-      user,
-    }
+    return { user }
   }
 }
