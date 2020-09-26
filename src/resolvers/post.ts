487f3f801fbce5e87e2dcb837b54874752d55745
tieulam <ttl.t954@gmail.com>
2020-09-26 14:17:19 -0600

    add delete post

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -43,4 +43,31 @@ export class PostResolver {
     }
     return post;
   }
+
+  // @Mutation(() => Boolean) 
+  // async deletePost(
+  //   @Arg('id') id: number, 
+  //   @Ctx() { em }: MyContext
+  // ) : Promise<boolean> {
+  //   try {
+  //     await em.nativeDelete(Post, { id })
+  //   } catch {
+  //     return false;
+  //   }
+  //   return true;
+  // }
+
+  @Mutation(() => Post, { nullable: true })
+  async deletePost(
+    @Arg('id') id:number,
+    @Ctx() { em }: MyContext
+  ) : Promise<Post | null> {
+    const post = await em.findOne(Post, { id });
+    if (post) {
+      await em.nativeDelete(Post, { id })
+      return post;
+    } else {
+      return null;
+    }
+  }
 }
