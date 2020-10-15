0d45829b134f7959982efe139e6a76a3546aa690
tieulam <ttl.t954@gmail.com>
2020-10-15 15:54:24 -0600

    add navbar

--- web/src/components/NavBar.tsx
+++ web/src/components/NavBar.tsx
@@ -1,13 +1,24 @@
-import { Box } from '@chakra-ui/core';
+import React from 'react';
+import { Flex, Box, Link } from '@chakra-ui/core';
+import NextLink from 'next/link';
 
-interface NavBarProps {
+interface NavBarProps {}
 
+{
+  /* nextlink uses client-side routing (preferred over anchor links) */
 }
 
 export const NavBar: React.FC<NavBarProps> = ({}) => {
   return (
-    <Box bg="tomato">
-
-    </Box>
+    <Flex bg="tomato" p={4}>
+      <Box ml={"auto"}>
+        <NextLink href="/login"> 
+          <Link color="white" mr={4}>Login</Link>
+        </NextLink>
+        <NextLink href="/register">
+          <Link color="white">Register</Link>
+        </NextLink>
+      </Box>
+    </Flex>
   )
 }
--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,5 +1,11 @@
+import React from "react"
+import { NavBar } from "../components/NavBar"
+
 const Index = () => (
-  <div>hello world</div>
+  <>
+    <NavBar />
+    <div>hello world</div>
+  </>
 )
 
 export default Index
