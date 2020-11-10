03890383606f6663577b1d28c45178491ed1e673
tieulam <ttl.t954@gmail.com>
2020-11-10 09:47:37 -0700

    refactor urql client function

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,81 +1,12 @@
-import { ThemeProvider, CSSReset, ColorModeProvider } from '@chakra-ui/core';
-import { cacheExchange, Cache, QueryInput } from '@urql/exchange-graphcache';
-import { Provider, createClient, fetchExchange, dedupExchange } from 'urql';
-import { MeDocument, LoginMutation, MeQuery, RegisterMutation, LogoutMutation } from '../generated/graphql';
+import { CSSReset, ThemeProvider } from '@chakra-ui/core';
 import theme from '../theme';
-
-// properly cast types 
-function betterUpdateQuery<Result, Query>(
-  cache: Cache, 
-  qi: QueryInput, 
-  result: any,
-  fn: (r: Result, q: Query) => Query
-) {
-  return cache.updateQuery(qi, data => fn(result, data as any) as any)
-}
-
-const client = createClient({ 
-  url: "http://localhost:4000/graphql",
-  fetchOptions: {
-    credentials: "include", //set to include cookies when log in 
-  },
-  exchanges: [dedupExchange, cacheExchange({
-    updates: {
-    Mutation: {
-      logout: (_result, args, cache, info) => {
-        betterUpdateQuery<LogoutMutation, MeQuery>(
-          cache, 
-          { query: MeDocument },
-          _result, 
-          () => ({ me: null })
-        );
-      },
-      login: (_result, args, cache, info) => {
-        betterUpdateQuery<LoginMutation, MeQuery>(
-          cache, 
-          { query: MeDocument }, 
-          _result, 
-          (result, query) => {
-            if (result.login.errors) {
-              return query;
-            } else {
-              return {
-                me: result.login.user, 
-              };
-            }
-          } 
-        );
-      },
-
-      register: (_result, args, cache, info) => {
-        betterUpdateQuery<RegisterMutation, MeQuery>(
-          cache, 
-          { query: MeDocument }, 
-          _result, 
-          (result, query) => {
-            if (result.register.errors) {
-              return query;
-            } else {
-              return {
-                me: result.register.user, 
-              };
-            }
-          } 
-        );
-      },
-    },
-  },
-  }), fetchExchange], // custom update to overwrite cached pages 
-})
 
 function MyApp({ Component, pageProps }: any) {
   return (
-    <Provider value={client}>
-      <ThemeProvider theme={theme}>
-          <CSSReset />
-          <Component {...pageProps} />
-      </ThemeProvider>
-    </Provider>
+    <ThemeProvider theme={theme}>
+        <CSSReset />
+        <Component {...pageProps} />
+    </ThemeProvider>
   );
 }
 
