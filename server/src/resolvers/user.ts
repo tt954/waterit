adf89d09f5e6dabb614cf39a4c49c433f2e0b540
tieulam <ttl.t954@gmail.com>
2020-11-12 22:49:38 -0700

    add forgotPassword mutation in UserResolver

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -41,6 +41,15 @@ class UserResponse {
 
 @Resolver()
 export class UserResolver {
+  @Mutation(() => Boolean)
+  async forgotPassword(
+    @Arg('email') email: string,
+    @Ctx() { em }: MyContext
+  ) {
+    // const user = await em.findOne(User, { email });
+    return true;
+  }
+
   @Query(() => User, { nullable: true })
   async me(
     @Ctx() { em, req }: MyContext
